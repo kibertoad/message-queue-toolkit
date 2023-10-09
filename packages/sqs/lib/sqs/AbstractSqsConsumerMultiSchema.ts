6683c43b0447c66db1421a542c38f200597983f1
CarlosGamero <carlos.gamero@lokalise.com>
2023-10-09 12:38:08 +0200

    SQS Passing barrierOutput to the handler

--- packages/sqs/lib/sqs/AbstractSqsConsumerMultiSchema.ts
+++ packages/sqs/lib/sqs/AbstractSqsConsumerMultiSchema.ts
@@ -72,10 +72,11 @@ export abstract class AbstractSqsConsumerMultiSchema<
   public override async processMessage(
     message: MessagePayloadType,
     messageType: string,
+    barrierOutput: unknown,
   ): Promise<Either<'retryLater', 'success'>> {
     const handler = this.handlerContainer.resolveHandler(messageType)
     // @ts-ignore
-    return handler.handler(message, this)
+    return handler.handler(message, this, barrierOutput)
   }
 
   protected override resolveMessageLog(message: MessagePayloadType, messageType: string): unknown {
