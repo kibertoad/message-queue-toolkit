5fb95715525516a5b16c231b0cfc932867d9dc3a
Igor Savin <iselwin@gmail.com>
2024-07-02 17:50:53 +0300

    Add util types for a PublishderMessageSchema (#183)

--- packages/schemas/lib/utils/messageTypeUtils.ts
+++ packages/schemas/lib/utils/messageTypeUtils.ts
@@ -8,3 +8,17 @@ import type { CommonEventDefinition } from '../events/eventTypes'
 export type ConsumerMessageSchema<MessageDefinitionType extends CommonEventDefinition> = z.infer<
   MessageDefinitionType['consumerSchema']
 >
+
+/**
+ * Resolves schema of a publisher message for a given event definition
+ */
+export type PublisherMessageSchema<MessageDefinitionType extends CommonEventDefinition> = z.infer<
+  MessageDefinitionType['publisherSchema']
+>
+
+/**
+ * Resolves schema of all possible publisher messages for a given list of event definitions
+ */
+export type allPublisherMessageSchemas<MessageDefinitionTypes extends CommonEventDefinition[]> = z.infer<
+    MessageDefinitionTypes[number]['publisherSchema']
+>
