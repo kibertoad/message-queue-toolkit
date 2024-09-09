14fbfba60499fae028fc51dfa3153726bd0be2a0
Kamil Wylegała <kamil.wylegala@gmail.com>
2024-09-09 15:49:31 +0200

    WIP prisma adapter.

--- packages/outbox-prisma-adapter/lib/outbox-prisma-adapter.ts
+++ packages/outbox-prisma-adapter/lib/outbox-prisma-adapter.ts
@@ -1,14 +1,24 @@
 import type { OutboxAccumulator, OutboxEntry } from '@message-queue-toolkit/outbox-core'
 import type { OutboxStorage } from '@message-queue-toolkit/outbox-core/dist/lib/storage'
 import type { CommonEventDefinition } from '@message-queue-toolkit/schemas'
+import type { PrismaClient } from '@prisma/client'
 
 export class OutboxPrismaAdapter<SupportedEvents extends CommonEventDefinition[]>
   implements OutboxStorage<SupportedEvents>
 {
+  constructor(
+    private readonly prisma: PrismaClient,
+    private readonly modelName: string,
+  ) {}
+
   createEntry(
     outboxEntry: OutboxEntry<SupportedEvents[number]>,
   ): Promise<OutboxEntry<SupportedEvents[number]>> {
-    return Promise.resolve(undefined)
+    const prismaModel: PrismaClient[typeof this.modelName] = this.prisma[this.modelName]
+
+    return prismaModel.create({
+      data: outboxEntry,
+    })
   }
 
   flush(outboxAccumulator: OutboxAccumulator<SupportedEvents>): Promise<void> {
