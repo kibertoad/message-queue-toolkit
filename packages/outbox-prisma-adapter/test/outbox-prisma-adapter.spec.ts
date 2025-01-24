0fc3e7f1ae571f583732120a939f9d85c09c5330
Kamil Wylegała <kamil.wylegala@gmail.com>
2025-01-24 11:25:59 +0100

    Type fix.

--- packages/outbox-prisma-adapter/test/outbox-prisma-adapter.spec.ts
+++ packages/outbox-prisma-adapter/test/outbox-prisma-adapter.spec.ts
@@ -38,7 +38,6 @@ describe('outbox-prisma-adapter', () => {
       timestamp: new Date().toISOString(),
       type: 'entity.created',
     },
-    type: 'entity.created',
     retryCount: 0,
     created: new Date(),
   } satisfies OutboxEntry<SupportedEvents[number]>
@@ -55,7 +54,6 @@ describe('outbox-prisma-adapter', () => {
       metadata: {},
       timestamp: new Date().toISOString(),
     },
-    type: 'entity.created',
     retryCount: 0,
     created: new Date(),
   } satisfies OutboxEntry<SupportedEvents[number]>
@@ -107,7 +105,6 @@ describe('outbox-prisma-adapter', () => {
         metadata: {},
         timestamp: new Date().toISOString(),
       },
-      type: 'entity.created',
       retryCount: 0,
       created: new Date(),
     } satisfies OutboxEntry<SupportedEvents[number]>)
