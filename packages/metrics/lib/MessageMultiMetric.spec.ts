14eab506878ee7c69d8cb12c083dcc959255cd8b
CarlosGamero <carlos.gamero@lokalise.com>
2025-03-05 17:58:47 +0100

    lint fix

--- packages/metrics/lib/MessageMultiMetric.spec.ts
+++ packages/metrics/lib/MessageMultiMetric.spec.ts
@@ -1,7 +1,7 @@
 import type { ProcessedMessageMetadata } from '@message-queue-toolkit/core'
 import * as promClient from 'prom-client'
 import { describe, expect, it } from 'vitest'
-import { MessageMultiMetric } from './MessageMultiMetrics'
+import { MessageMultiMetric } from './MessageMultiMetric'
 import { MessageLifetimeMetric } from './prometheus/metrics/MessageLifetimeMetric'
 import { MessageProcessingTimeMetric } from './prometheus/metrics/MessageProcessingTimeMetric'
 
