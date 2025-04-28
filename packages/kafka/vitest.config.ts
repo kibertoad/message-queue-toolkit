f671acd05074944be9e78a7bcdb45b8841f06360
CarlosGamero <carlos.gamero@lokalise.com>
2025-04-28 13:14:04 +0200

    TECH-894 bootstrapping kafka package

---
+++ packages/kafka/vitest.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitest/config'
+
+// biome-ignore lint/style/noDefaultExport: vite expects default export
+export default defineConfig({
+  test: {
+    globals: true,
+    watch: false,
+    restoreMocks: true,
+    pool: 'threads',
+    coverage: {
+      provider: 'v8',
+      include: ['lib/**/*.ts'],
+      exclude: ['vitest.config.ts', 'lib/**/index.ts'],
+      thresholds: {
+        lines: 100,
+        functions: 100,
+        branches: 100,
+        statements: 100,
+      },
+    },
+  },
+})
