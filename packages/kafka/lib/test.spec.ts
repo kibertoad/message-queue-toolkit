accd3105e91607f75385c29e21b85f74b06675e4
CarlosGamero <carlos.gamero@lokalise.com>
2025-04-29 16:40:25 +0200

    Adding kafka test

--- packages/kafka/lib/test.spec.ts
+++ packages/kafka/lib/test.spec.ts
@@ -1,4 +1,6 @@
-import { features, librdkafkaVersion } from 'node-rdkafka'
+import { once } from 'node:events'
+import { waitAndRetry } from '@lokalise/universal-ts-utils/node'
+import { KafkaConsumer, type Message, Producer, features, librdkafkaVersion } from 'node-rdkafka'
 
 // TODO: to be removed once we have proper tests
 describe('Checking node-rdkafka', () => {
@@ -18,4 +20,61 @@ describe('Checking node-rdkafka', () => {
     `)
     expect(librdkafkaVersion).toMatchInlineSnapshot(`"2.8.0"`)
   })
+
+  it(
+    'should send and receive a message',
+    async () => {
+      // Given
+      const brokers = 'localhost:9092'
+      const topic = 'test-topic'
+      const messageValue = 'My test message'
+
+      const receivedMessages: Message[] = []
+
+      // Create a producer
+      const producer = new Producer({
+        'metadata.broker.list': brokers,
+        'allow.auto.create.topics': true,
+      })
+      producer.connect()
+      await once(producer, 'ready')
+
+      // Create a consumer
+      const consumer = new KafkaConsumer(
+        {
+          'group.id': 'test-group',
+          'metadata.broker.list': brokers,
+          'allow.auto.create.topics': true,
+        },
+        {},
+      )
+      consumer.connect()
+
+      await new Promise((resolve, reject) =>
+        consumer
+          .on('ready', () => {
+            consumer.subscribe([topic]).consume()
+            resolve(resolve)
+          })
+          .on('event.error', (err) => reject(err))
+          .on('data', (data) => {
+            receivedMessages.push(data)
+          }),
+      )
+
+      // When
+      producer.produce(topic, null, Buffer.from(messageValue))
+      producer.flush()
+
+      // Then
+      await waitAndRetry(() => receivedMessages.length > 0, 10, 1000)
+      expect(receivedMessages).toHaveLength(1)
+      expect(receivedMessages[0]?.value?.toString()).toBe(messageValue)
+
+      // Cleanup
+      producer.disconnect()
+      consumer.disconnect()
+    },
+    { timeout: 20000 },
+  )
 })
