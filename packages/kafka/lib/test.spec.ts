75c30561dc51924f3987e936e14ed557f8b5dbdb
CarlosGamero <carlos.gamero@lokalise.com>
2025-04-29 18:03:50 +0200

    Fix vitest warning

--- packages/kafka/lib/test.spec.ts
+++ packages/kafka/lib/test.spec.ts
@@ -3,8 +3,8 @@ import { waitAndRetry } from '@lokalise/universal-ts-utils/node'
 import { KafkaConsumer, type Message, Producer, features, librdkafkaVersion } from 'node-rdkafka'
 
 // TODO: to be removed once we have proper tests
-describe('Checking node-rdkafka', () => {
-  it('should work', () => {
+describe('Test', () => {
+  it('should use node-rdkafka', () => {
     expect(features).toMatchInlineSnapshot(`
       [
         "gzip",
@@ -21,63 +21,59 @@ describe('Checking node-rdkafka', () => {
     expect(librdkafkaVersion).toMatchInlineSnapshot(`"2.8.0"`)
   })
 
-  it(
-    'should send and receive a message',
-    async () => {
-      // Given
-      const brokers = 'localhost:9092'
-      const topic = 'test-topic'
-      const messageValue = 'My test message'
+  it('should send and receive a message', { timeout: 20000 }, async () => {
+    // Given
+    const brokers = 'localhost:9092'
+    const topic = 'test-topic'
+    const messageValue = 'My test message'
 
-      const receivedMessages: Message[] = []
+    const receivedMessages: Message[] = []
 
-      // Create a producer
-      const producer = new Producer({
+    // Create a producer
+    const producer = new Producer({
+      'metadata.broker.list': brokers,
+      'allow.auto.create.topics': true,
+    })
+    producer.connect()
+    await once(producer, 'ready')
+
+    // Create a consumer
+    const consumer = new KafkaConsumer(
+      {
+        'group.id': `test-group-${Date.now()}`,
         'metadata.broker.list': brokers,
         'allow.auto.create.topics': true,
-      })
-      producer.connect()
-      await once(producer, 'ready')
+      },
+      {
+        'auto.offset.reset': 'latest',
+      },
+    )
+    consumer.connect()
 
-      // Create a consumer
-      const consumer = new KafkaConsumer(
-        {
-          'group.id': `test-group-${Date.now()}`,
-          'metadata.broker.list': brokers,
-          'allow.auto.create.topics': true,
-        },
-        {
-          'auto.offset.reset': 'latest',
-        },
-      )
-      consumer.connect()
+    await new Promise<void>((resolve, reject) =>
+      consumer
+        .on('ready', () => {
+          consumer.subscribe([topic])
+          consumer.consume()
+          resolve()
+        })
+        .on('event.error', (err) => reject(err))
+        .on('data', (data) => {
+          receivedMessages.push(data)
+        }),
+    )
 
-      await new Promise<void>((resolve, reject) =>
-        consumer
-          .on('ready', () => {
-            consumer.subscribe([topic])
-            consumer.consume()
-            resolve()
-          })
-          .on('event.error', (err) => reject(err))
-          .on('data', (data) => {
-            receivedMessages.push(data)
-          }),
-      )
+    // When
+    producer.produce(topic, null, Buffer.from(messageValue))
+    producer.flush()
 
-      // When
-      producer.produce(topic, null, Buffer.from(messageValue))
-      producer.flush()
+    // Then
+    await waitAndRetry(() => receivedMessages.length > 0, 10, 1500)
+    expect(receivedMessages).toHaveLength(1)
+    expect(receivedMessages[0]?.value?.toString()).toBe(messageValue)
 
-      // Then
-      await waitAndRetry(() => receivedMessages.length > 0, 10, 1500)
-      expect(receivedMessages).toHaveLength(1)
-      expect(receivedMessages[0]?.value?.toString()).toBe(messageValue)
-
-      // Cleanup
-      producer.disconnect()
-      consumer.disconnect()
-    },
-    { timeout: 20000 },
-  )
+    // Cleanup
+    producer.disconnect()
+    consumer.disconnect()
+  })
 })
