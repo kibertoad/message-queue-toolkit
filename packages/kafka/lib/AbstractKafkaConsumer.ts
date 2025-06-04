9547484f571bda8dd6e50800f141f700ba5a6434
CarlosGamero <carlos.gamero@lokalise.com>
2025-06-04 15:17:19 +0200

    Adding in memory retry

--- packages/kafka/lib/AbstractKafkaConsumer.ts
+++ packages/kafka/lib/AbstractKafkaConsumer.ts
@@ -1,3 +1,4 @@
+import { setTimeout } from 'node:timers/promises'
 import { InternalError, stringValueSerializer } from '@lokalise/node-core'
 import {
   type ConsumeOptions,
@@ -11,6 +12,7 @@ import {
 import { AbstractKafkaService, type BaseKafkaOptions } from './AbstractKafkaService.ts'
 import { KafkaHandlerContainer } from './handler-container/KafkaHandlerContainer.js'
 import type { KafkaHandlerRouting } from './handler-container/KafkaHandlerRoutingBuilder.js'
+import type { KafkaHandler } from './handler-container/index.js'
 import type { KafkaDependencies, TopicConfig } from './types.ts'
 
 export type KafkaConsumerOptions<TopicsConfig extends TopicConfig[]> = BaseKafkaOptions &
@@ -21,6 +23,8 @@ export type KafkaConsumerOptions<TopicsConfig extends TopicConfig[]> = BaseKafka
   Omit<ConsumeOptions<string, object, string, object>, 'topics'> & {
     handlers: KafkaHandlerRouting<TopicsConfig>
   }
+
+const MAX_IN_MEMORY_RETRIES = 3
 
 export abstract class AbstractKafkaConsumer<
   TopicsConfig extends TopicConfig[],
@@ -42,7 +46,6 @@ export abstract class AbstractKafkaConsumer<
       ...this.options.kafka,
       ...this.options,
       autocommit: false, // Handling commits manually
-      connectTimeout: 1,
       deserializers: {
         key: stringDeserializer,
         value: jsonDeserializer,
@@ -90,9 +93,9 @@ export abstract class AbstractKafkaConsumer<
     // if there is no handler for the message, we ignore it (simulating subscription)
     if (!handler) return message.commit()
 
-    const parsedMessageValue = handler.schema.safeParse(message.value)
-    if (!parsedMessageValue.success) {
-      this.handlerError(parsedMessageValue.error, { message: stringValueSerializer(message) })
+    const parseResult = handler.schema.safeParse(message.value)
+    if (!parseResult.success) {
+      this.handlerError(parseResult.error, { message: stringValueSerializer(message) })
       this.handleMessageProcessed({
         message: message,
         processingResult: { status: 'error', errorReason: 'invalidMessage' },
@@ -102,22 +105,48 @@ export abstract class AbstractKafkaConsumer<
       return message.commit()
     }
 
-    try {
-      await handler.handler({ ...message, value: parsedMessageValue.data })
+    const validatedMessage = parseResult.data
+
+    let retries = 0
+    let consumed = false
+    do {
+      // exponential backoff -> 2^(retry-1)
+      if (retries > 0) await setTimeout(Math.pow(2, retries - 1))
+
+      consumed = await this.tryToConsume({ ...message, value: validatedMessage }, handler.handler)
+      if (consumed) break
+
+      retries++
+    } while (retries < MAX_IN_MEMORY_RETRIES)
+
+    if (consumed) {
       this.handleMessageProcessed({
-        message: message,
+        message: validatedMessage,
         processingResult: { status: 'consumed' },
         topic: message.topic,
       })
-
-      return message.commit()
-    } catch (error) {
-      this.handlerError(error, { message: stringValueSerializer(message) })
+    } else {
       this.handleMessageProcessed({
-        message: message,
+        message: validatedMessage,
         processingResult: { status: 'error', errorReason: 'handlerError' },
         topic: message.topic,
       })
     }
+
+    return message.commit()
+  }
+
+  private async tryToConsume<MessageValue extends object>(
+    message: Message<string, MessageValue, string, object>,
+    handler: KafkaHandler<MessageValue>,
+  ): Promise<boolean> {
+    try {
+      await handler(message)
+      return true
+    } catch (error) {
+      this.handlerError(error, { message: stringValueSerializer(message.value) })
+    }
+
+    return false
   }
 }
