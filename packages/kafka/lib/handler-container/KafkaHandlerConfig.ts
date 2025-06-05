d5cef9bc5b07185d2c49c6a2d1fa5c597de18c1e
CarlosGamero <carlos.gamero@lokalise.com>
2025-06-05 12:05:44 +0200

    Adding request context to handlers

--- packages/kafka/lib/handler-container/KafkaHandlerConfig.ts
+++ packages/kafka/lib/handler-container/KafkaHandlerConfig.ts
@@ -1,8 +1,16 @@
+import type { CommonLogger } from '@lokalise/node-core'
 import type { Message } from '@platformatic/kafka'
 import type { ZodSchema } from 'zod'
 
+// TODO: not sure if better to use fastify-extras
+export interface RequestContext {
+  logger: CommonLogger
+  reqId: string
+}
+
 export type KafkaHandler<MessageValue extends object> = (
   message: Message<string, MessageValue, string, object>,
+  requestContext: RequestContext,
 ) => Promise<void> | void
 
 export class KafkaHandlerConfig<MessageValue extends object> {
