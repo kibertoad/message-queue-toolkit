6f17123c878f21dafd35868fa1afb4204390023c
CarlosGamero <carlos.gamero@lokalise.com>
2025-06-06 10:21:18 +0200

    Removing todo

--- packages/kafka/lib/handler-container/KafkaHandlerConfig.ts
+++ packages/kafka/lib/handler-container/KafkaHandlerConfig.ts
@@ -2,7 +2,6 @@ import type { CommonLogger } from '@lokalise/node-core'
 import type { Message } from '@platformatic/kafka'
 import type { ZodSchema } from 'zod'
 
-// TODO: not sure if better to use fastify-extras
 export interface RequestContext {
   logger: CommonLogger
   reqId: string
